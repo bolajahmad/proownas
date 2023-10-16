5117e94912ce0afe36e14259f7c2227caafdc88b
Bolaji Ahmad <56865496+bolajahmad@users.noreply.github.com>
2023-10-16 22:14:43 +0100

    update: implemented voting mechanism on votes

---
+++ packages/contracts/README.md
@@ -0,0 +1,46 @@
+# Proownas DAO Contract
+
+---
+## What is Proownas
+
+ Proownas is a community of real-estate owners. The goal is to allow an economy where real-estate owners can mutually benefit from the propoerties they own. It will allow owners to earn more from their properties and also have access to more properties without having to pay for them. We do this by minting real-world real-estate assets into NFTs.
+---
+
+---
+## What does Proownas DAO do?
+
+The purpose of the DAO is to collectively make decisions on properties, membership, values. Initially, the DAO is made up of few members who own all the propoerties. These members are able to review the qualifications of a new member. The DAO is tasked to verify that new member is real and can be located in the real world. To verify the existence of the properties described in the proposal and also confirm ownership of it. The DAO is also responsible minting new assets on-chain.
+---
+
+### Responsibilities
+
+- Prove existence of a new member, and accept new member to the DAO
+- Blacklist/Whitelist a member
+- Vote on proposals (Proposals can be for submitting new asset which will also create a new user, submitting an opinion on what to build etc.)
+- Verify existence of properties and link ownership to member(s)
+- Mint properties on-chain as an NFT
+- Determine the value of NFT (linked to the real-world asset.) and also
+- Decide on the reward percentage and earning ratio
+
+
+## Contracts
+
+### The DAO
+
+#### Storage
+ ```
+    #[ink(storage)]
+    #[derive(Default)]
+    pub struct DAO {
+        cid_by_proposal_id: Mapping<u128, Vec<u8>>,
+        proposals_by_id: Mapping<u128, Proposal>,
+        proposals_by_account: Mapping<AccountId, Vec<u128>>,
+        proposal_count: u64,
+        assets_owned: Mapping<Vec<u8>, AccountId>,
+    }
+```
+
+####  Messages
+
+**Submit new asset**
+Allows user to register. Technically, you're submitting your asset cid along with your wallet address, and the proposal count will be incremented as every action is a proposal. 
--- packages/contracts/dao/lib.rs
+++ packages/contracts/dao/lib.rs
@@ -8,12 +8,30 @@ mod dao {
     use scale::{Decode, Encode};
 
     #[ink(event)]
-    pub struct ProposaUpdated {}
+    pub struct ProposalUpdated {
+        #[ink(topic)]
+        owner: AccountId,
+        proposal_cid: Vec<u8>,
+        updated_at: u32,
+        proposal_id: u128,
+    }
+
+    #[ink(event)]
+    pub struct VoteUpdated {
+        #[ink(topic)]
+        voter: AccountId,
+        updated_at: u32,
+        proposal_id: u128,
+        votes_for: Option<u64>,
+        votes_against: Option<u64>,
+    }
 
     #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub enum Error {
+        AlreadyVoted,
         InvalidAsset,
+        ProposalNotFound,
         AsserExists,
     }
 
@@ -24,6 +42,7 @@ mod dao {
     )]
     pub enum ProposalStatus {
         Pending,
+        Ongoing,
         Approved,
         Rejected,
     }
@@ -50,15 +69,26 @@ mod dao {
         start_block: u32,
         duration: u32,
         proposal_cid: Vec<u8>,
-        proposal_id: u64,
+        proposal_id: u128,
     }
 
+    #[derive(Encode, Decode)]
+    #[cfg_attr(
+        feature = "std",
+        derive(Debug, Clone, PartialEq, Eq, scale_info::TypeInfo, StorageLayout,)
+    )]
+    pub struct Vote {
+        votes_for: Option<u64>,
+        votes_against: Option<u64>,
+    }
     #[ink(storage)]
     #[derive(Default)]
     pub struct DAO {
-        proposals_list: Vec<Proposal>,
-        proposals_by_account: Mapping<AccountId, Vec<u64>>,
-        proposal_count: u64,
+        cid_by_proposal_id: Mapping<u128, Vec<u8>>,
+        proposal_by_id: Mapping<u128, Proposal>,
+        proposals_by_account: Mapping<AccountId, Vec<u128>>,
+        proposal_count: u128,
+        votes_by_proposal: Mapping<(u128, AccountId), Vote>,
         assets_owned: Mapping<Vec<u8>, AccountId>,
     }
 
@@ -70,27 +100,30 @@ mod dao {
             let caller = Self::env().caller();
             assets_owned.insert(initial_assets, &caller);
             Self {
-                proposals_list: Vec::new(),
+                cid_by_proposal_id: Mapping::new(),
+                proposal_by_id: Mapping::new(),
                 proposals_by_account: Mapping::new(),
                 proposal_count: 0,
+                votes_by_proposal: Mapping::new(),
                 assets_owned,
             }
         }
 
         /// Submit a new proposal to the DAO
+        /// anyone can call this function to submit a proposal for asset
         /// @param proposal_cid: IPFS CID of the proposal.
         #[ink(message)]
-        pub fn submit_new_proposal(&mut self, proposal_cid: Vec<u8>) -> Result<()> {
-            let caller = self.env().caller();
+        pub fn submit_new_asset(&mut self, proposal_cid: Vec<u8>) -> Result<()> {
+            let caller: AccountId = self.env().caller();
             let mut proposals_of = self.proposals_by_account.get(&caller).unwrap_or(Vec::new());
 
-            let proposal_exists = self
-                .proposals_list
-                .iter()
-                .any(|p| p.proposal_cid == proposal_cid);
-
+            let proposal_exists = match &self.assets_owned.get(&proposal_cid) {
+                Some(_) => true,
+                None => false,
+            };
             // proposal_cid must be existent, need to update the proposal in that case.
             assert!(!proposal_exists, "Proposal exists");
+            self.assets_owned.insert(&proposal_cid, &caller);
 
             let mut proposal_count = self.proposal_count;
             proposal_count += 1;
@@ -102,36 +135,137 @@ mod dao {
                 status: ProposalStatus::Pending,
                 duration: 10,
                 start_block: self.env().block_number(),
-                proposal_cid,
+                proposal_cid: proposal_cid.clone(),
                 proposal_id: proposal_count,
             };
 
-            self.proposals_list.push(proposal);
             self.proposal_count = proposal_count;
+            self.cid_by_proposal_id
+                .insert(&proposal_count, &proposal_cid);
+            self.proposal_by_id.insert(&proposal_count, &proposal);
+
+            self.env().emit_event(ProposalUpdated {
+                owner: caller,
+                proposal_cid,
+                proposal_id: proposal_count,
+                updated_at: self.env().block_number(),
+            });
             Ok(())
         }
 
         /// Open proposal for voting
         /// This should be done when the user is verified
         #[ink(message)]
-        pub fn update_proposal_status(&mut self, proposal_id: u64) -> Result<()> {
+        pub fn activate_voting(&mut self, proposal_id: u128) -> Result<()> {
             let caller = self.env().caller();
-            let proposal = self
-                .proposals_list
-                .iter_mut()
-                .find(|p| p.proposal_id == proposal_id)
-                .ok_or(Error::InvalidAsset)?;
+            assert!(proposal_id <= self.proposal_count, "Invalid Proposal ID");
+            let proposal_option = self.proposal_by_id.get(&proposal_id);
 
-            proposal.status = ProposalStatus::Approved;
-            self.pro
+            if let Some(mut proposal) = proposal_option {
+                let can_update_proposal = match proposal.status {
+                    ProposalStatus::Pending => true,
+                    _ => false,
+                };
+                assert!(can_update_proposal, "Proposal is active",);
+
+                let caller_proposals = match self.proposals_by_account.get(&caller) {
+                    Some(ps) => ps,
+                    None => Vec::new(),
+                };
+                assert!(caller_proposals.len() > 0, "No proposals available");
+
+                let current_proposal = caller_proposals.iter().find(|p| **p == proposal_id);
+
+                if current_proposal.is_some() {
+                    proposal.status = ProposalStatus::Ongoing;
+                    self.proposal_by_id.insert(&proposal_id, &proposal);
+                    self.env().emit_event(ProposalUpdated {
+                        owner: caller,
+                        proposal_cid: proposal.proposal_cid,
+                        proposal_id,
+                        updated_at: self.env().block_number(),
+                    });
+
+                    self.votes_by_proposal.insert(
+                        &(proposal_id, caller),
+                        &Vote {
+                            votes_for: Some(0),
+                            votes_against: Some(0),
+                        },
+                    );
+                }
+
+                Ok(())
+            } else {
+                Err(Error::InvalidAsset)
+            }
+        }
+
+        #[ink(message)]
+        pub fn vote_on_proposal(&mut self, proposal_id: u128, vote: VoteType) -> Result<()> {
+            let caller = self.env().caller();
+            let block_number = self.env().block_number();
+            let proposal = self.proposal_by_id.get(&proposal_id).unwrap();
+            assert!(
+                proposal.status == ProposalStatus::Ongoing
+                    && (proposal.start_block + proposal.duration > block_number),
+                "ClosedProposal"
+            );
+
+            let to_vote_on = self.votes_by_proposal.get(&(proposal_id, caller)).unwrap();
+            let new_votes = match vote {
+                VoteType::Yes(_) => {
+                    let votes_for = match to_vote_on.votes_for {
+                        Some(value) => value + 1,
+                        None => 1,
+                    };
+                    let new_vote = Vote {
+                        votes_for: Some(votes_for),
+                        votes_against: Some(to_vote_on.votes_against.unwrap()),
+                    };
+                    new_vote
+                }
+                VoteType::No(_) => {
+                    let votes_against = match to_vote_on.votes_against {
+                        Some(value) => value + 1,
+                        None => 1,
+                    };
+                    let new_vote = Vote {
+                        votes_for: Some(to_vote_on.votes_for.unwrap()),
+                        votes_against: Some(votes_against),
+                    };
+                    new_vote
+                }
+            };
+
+            self.votes_by_proposal
+                .insert(&(proposal_id, caller), &new_votes);
+
+            self.env().emit_event(VoteUpdated {
+                voter: caller,
+                proposal_id,
+                updated_at: block_number,
+                votes_for: new_votes.votes_for,
+                votes_against: new_votes.votes_against,
+            });
+
             Ok(())
         }
 
         #[ink(message)]
-        pub fn get_proposal_by_id(&self, proposal_id: u64) -> bool {
-            self.proposals_list
-                .iter()
-                .any(|p| p.proposal_id == proposal_id)
+        pub fn get_proposal_by_id(&self, proposal_id: u128) -> Result<Proposal> {
+            match self.proposal_by_id.get(&proposal_id) {
+                Some(proposal) => Ok(proposal),
+                None => Err(Error::ProposalNotFound),
+            }
+        }
+
+        #[ink(message)]
+        pub fn get_proposal_ipfs_data(&self, proposal_id: u128) -> Result<Vec<u8>> {
+            match self.cid_by_proposal_id.get(&proposal_id) {
+                Some(cid) => Ok(cid),
+                None => Err(Error::ProposalNotFound),
+            }
         }
     }
     // #[cfg(test)]
